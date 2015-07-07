5547cb9ce8a5bc3f86e41f2b0945abf57f2c13f6
gaojinhsu <gaojin.hsu@gmail.com>
2015-07-07 17:36:18 +0800

    add audio recorder

---
+++ GJAudioPlayer/GJStreamingAudioRecorder.h
@@ -0,0 +1,13 @@
+//
+//  GJStreamingAudioRecorder.h
+//  GJAudioPlayerDemo
+//
+//  Created by Gaojin Hsu on 7/7/15.
+//  Copyright (c) 2015 gensee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GJStreamingAudioRecorder : NSObject
+
+@end
---
+++ GJAudioPlayer/GJStreamingAudioRecorder.m
@@ -0,0 +1,13 @@
+//
+//  GJStreamingAudioRecorder.m
+//  GJAudioPlayerDemo
+//
+//  Created by Gaojin Hsu on 7/7/15.
+//  Copyright (c) 2015 gensee. All rights reserved.
+//
+
+#import "GJStreamingAudioRecorder.h"
+
+@implementation GJStreamingAudioRecorder
+
+@end
